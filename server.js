8a025b6ff668b70676beaec238cf635e7738574b
Yossef N <y.naccache@agence-senzo.fr>
2019-07-30 15:30:09 +0200

    BDD info

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-shallow-4385',
       user : 'postgres',
       password : 'root',
       database : 'smart-brain'
