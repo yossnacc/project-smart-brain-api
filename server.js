0f70b6ae673b4fa7d14f6f1e15142dd08cbf0d20
Yossef N <y.naccache@agence-senzo.fr>
2019-07-30 15:33:52 +0200

    adding database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-shallow-4385',
-      user : 'postgres',
-      password : 'root',
-      database : 'smart-brain'
+      host : process.env.DATABASE_URL,
+      ssl: true,
     }
   });
 
